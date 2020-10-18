4292b42712e945b72b15e6c8cf3acf4d3172adc7
Lukasz <kuchtalukasz89@gmail.com>
2020-10-18 23:14:57 +0200

    Added MovieRepository

--- repository/src/main/java/pl/cinemaproject/repository/MovieRepository.java
+++ repository/src/main/java/pl/cinemaproject/repository/MovieRepository.java
@@ -1,4 +1,15 @@
 package pl.cinemaproject.repository;
 
-public interface MovieRepository {
+import pl.cinemaproject.persistence.model.Movie;
+import pl.cinemaproject.repository.generic.CrudRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+
+
+    Optional<Movie> findByName(String movieName);
+    Optional<Movie> findByDescription(String description);
+    Optional<Movie> findByStartTime(LocalDate startTime);
 }
